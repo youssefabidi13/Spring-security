22871e180b2fc4e216113e2b35b835667f3e15c0
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-03-05 23:51:12 +0100

    first commit

---
+++ section 1/springsecuritybasic/src/main/java/com/youssefabidi/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.youssefabidi.springsecuritybasic.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring Application with Security ";
+    }
+}
