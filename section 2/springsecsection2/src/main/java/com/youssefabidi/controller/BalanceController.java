2998c68b2d90a6efab61fc8483a0959489848c9b
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-03-06 13:08:46 +0100

    section 2

--- section 2/springsecsection2/src/main/java/com/youssefabidi/controller/BalanceController.java
+++ section 2/springsecsection2/src/main/java/com/youssefabidi/controller/BalanceController.java
@@ -1,2 +1,12 @@
-package com.youssefabidi.controller;public class BalanceController {
+package com.youssefabidi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+    @GetMapping("/myBalance")
+    public String getBalanceDetails(){
+        return "Here are the balance details from the DB";
+    }
 }
--- section 2/springsecsection2/src/main/java/com/youssefabidi/controller/LoansController.java
+++ section 2/springsecsection2/src/main/java/com/youssefabidi/controller/LoansController.java
@@ -1,2 +1,12 @@
-package com.youssefabidi.controller;public class LoansController {
+package com.youssefabidi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+    @GetMapping("/myLoans")
+    public String getBalanceDetails(){
+        return "Here are the loan details from the DB";
+    }
 }
