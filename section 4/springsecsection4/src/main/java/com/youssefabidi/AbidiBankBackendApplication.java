cdd7171efbe19c34adae635c3e70871df6312fd0
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-03-10 17:35:10 +0100

    Password Encoder

---
+++ section 4/springsecsection4/src/main/java/com/youssefabidi/AbidiBankBackendApplication.java
@@ -0,0 +1,17 @@
+ package com.youssefabidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+public class AbidiBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbidiBankBackendApplication.class, args);
+	}
+
+}
---
+++ section 4/springsecsection4/src/main/java/com/youssefabidi/controller/ContactController.java
@@ -0,0 +1,12 @@
+package com.youssefabidi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+    @GetMapping("/contact")
+    public String saveContactInquiryDetails(){
+        return "Inquiry  details are saved to the DB";
+    }
+}
