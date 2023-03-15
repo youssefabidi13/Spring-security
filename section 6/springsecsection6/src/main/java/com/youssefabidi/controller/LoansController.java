69ef57a3ec78c8b6af11f78f1379d4ed74edd4c2
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-03-15 20:04:57 +0100

    CORS AND CSRF FILTERS

---
+++ section 6/springsecsection6/src/main/java/com/youssefabidi/controller/LoansController.java
@@ -0,0 +1,28 @@
+package com.youssefabidi.controller;
+
+import com.youssefabidi.model.Loans;
+import com.youssefabidi.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LoansController {
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @GetMapping("/myLoans")
+    public List<Loans> getLoanDetails(@RequestParam int id) {
+        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
+        if (loans != null ) {
+            return loans;
+        }else {
+            return null;
+        }
+    }
+
+}
