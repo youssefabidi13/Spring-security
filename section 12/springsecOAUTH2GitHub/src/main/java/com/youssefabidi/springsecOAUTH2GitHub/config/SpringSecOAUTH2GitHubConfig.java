6187baf187a28789ad98b4f244fdfb15f84aadc3
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-03-18 21:29:28 +0100

    OAUTH2  using Github Auth server

---
+++ section 12/springsecOAUTH2GitHub/src/main/java/com/youssefabidi/springsecOAUTH2GitHub/config/SpringSecOAUTH2GitHubConfig.java
@@ -0,0 +1,34 @@
+package com.youssefabidi.springsecOAUTH2GitHub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecOAUTH2GitHubConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated().and().oauth2Login();
+        return http.build();
+    }
+
+    /*@Bean
+    public ClientRegistrationRepository clientRepository() {
+        ClientRegistration clientReg = clientRegistration();
+        return new InMemoryClientRegistrationRepository(clientReg);
+    }
+
+    private ClientRegistration clientRegistration() {
+		return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("fe77bd90404bb0f8cc1c")
+	           .clientSecret("9c79f8e4936b857744b1ce6d2ecb959d315646a4").build();
+	 }
+
+     */
+
+}
